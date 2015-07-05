a7231f056c70ed76c768fd676c414d9531764910
Heni <tunisienheni@gmail.com[gui]>
2015-07-05 02:38:29 +0200

    heni

---
+++ app/Controllers/homeCtrl.js
@@ -0,0 +1,62 @@
+app.controller("homeCtrl", function ($scope,$http) {
+	var root = 'http://jsonplaceholder.typicode.com';
+	$scope.fakeData=[];
+	$http.get(root + '/posts').
+		  success(function(data, status, headers, config) {
+		   	$scope.fakeData=data;
+		  }).
+		  error(function(data, status, headers, config) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+
+
+
+	if($scope.page == 'Index')
+		$scope.page = 'home';
+
+	$scope.listPersons=[
+	{'name':'heni','age':21, 'prenom':'mezzeni'},
+	{'name':'riz', 'age':0},
+	{'name':'téléphone', 'age':2},
+	{'name':'ahmed','prenom':'high tech',  'age':5},
+	{'name':'heni','age':21, 'prenom':'Fezzeni'},
+	{'name':'heni','age':21, 'prenom':'aezzeni'},
+	{'name':'heeni','age':6, 'prenom':'Fezrtzeni'},
+	{'name':'heni','age':12, 'prenom':'sezzueni'},
+	{'name':'heni','age':21, 'prenom':'rFezzeni'},
+	{'name':'hertni','age':21, 'prenom':'Fezzeni'},
+	{'name':'bfni','age':8, 'prenom':'kezsdzeni'},
+	{'name':'poni','age':21, 'prenom':'bFezzeni'},
+	{'name':'gusi','age':35,'prenom':'Fezzeni87'},
+	{'name':'hbsdni','age':22,'prenom':'Fez78zeni'},
+	{'name':'hesdni','age':22,'prenom':'ez78eni'},
+	{'name':'hesdni','age':22,'prenom':'Fezzeni'},
+	{'name':'htedni','age':22,'prenom':'yr78zeni'},
+	{'name':'redni','age':22,'prenom':'Fez78zeni'},
+	{'name':'hgsdni','age':22,'prenom':'Fre8zeni'},
+	{'name':'opusdni','age':44,'prenom':'sfz78zeni'},
+	{'name':'jusdni','age':57,'prenom':'sfz78zeni'},
+	{'name':'pusdni','age':78,'prenom':'sfz78zeni'},
+	{'name':'lousdni','age':28,'prenom':'sfz78zeni'}]
+
+	$scope.options={
+		search:{
+			searchText:'', 
+			searchClass:'form-control'
+		},
+		pagination:{
+			pageLength:20,
+			pageIndex:0,
+			pager:{
+				nextTitle:'Suivant',
+				perviousTitle:'Précédent'
+			},
+			itemsPerPage:{
+				selected:20,
+				range:[20,50,100]
+			}
+		}
+	};
+
+});
---
+++ app/Directives/global.js
@@ -0,0 +1,155 @@
+'use strict';
+
+app.directive("panel", function () {
+return {
+	link: function (scope, element, attrs) {
+		scope.dataSource = "directive";
+	},
+	restrict: "E",
+	scope: true,
+	template: function () {
+		return angular.element(
+		document.querySelector("#template")).html();
+	},
+	transclude: true
+	}
+}).directive("hfGrid", function () {
+	return {
+		restrict: "E",
+		scope: {
+			source:'=ngModel',
+			gridClass:'@', 
+			options:'='
+	},
+	templateUrl: 'template/blsGrid/blsGrid.html',
+	controller:function($scope,$filter,$timeout){
+			$scope.options=$scope.options||{
+				search:{
+					searchText:'', 
+					searchClass:'form-control'
+				},
+				pagination:{
+					pageLength:5,
+					pageIndex:0,
+					pager:{
+						nextTitle:'Suivant',
+						perviousTitle:'Précédent'
+					},
+					itemsPerPage:{
+						selected:10,
+						range:[10,20]
+					}
+				}
+			};
+			$scope.columns=[];
+			$scope.isLoading=true;
+			$scope.dataFilterSearch = $scope.data=[];
+			$scope.offset=0;
+			$scope.filteredData =[];
+			$scope.$watch('source.length',function(newVal, oldValue)
+				{
+					angular.forEach($scope.source, function(value, key){
+						$scope.data.push(value);
+						//if(key===0)
+						//	$scope.columns=Object.keys(value);
+						angular.forEach(value,function(v, k){
+						if($scope.columns.indexOf(k)<0)
+							$scope.columns.push(k);
+						});
+					});
+					$scope.reverse = true;
+					$scope.predicate= $scope.columns[0];
+					$scope.pages= new Array(Math.ceil($scope.data.length/$scope.options.pagination.pageLength));
+					if($scope.options.pagination.itemsPerPage && $scope.options.pagination.itemsPerPage.range && $scope.options.pagination.itemsPerPage.range.indexOf($scope.options.pagination.pageLength)<1)
+            						$scope.options.pagination.pageLength=$scope.options.pagination.itemsPerPage.range[0];
+            					$scope.isLoading=false;
+				});
+			
+			$scope.order = function(predicate) {
+	        			$scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+	        			$scope.predicate = predicate;
+	      		};
+	      		$scope.glyphOrder= function(col){
+	      			if(col!=$scope.predicate)
+	      				return '';
+	      			return $scope.reverse? 'glyphicon-chevron-up':'glyphicon-chevron-down';
+	      		};
+	      		$scope.toPage=function  (page) {
+	      			$scope.options.pagination.pageIndex=page;
+	      			$scope.refreshOffset();
+	      		}
+	      		$scope.$watch('options.pagination.pageIndex', function(newValue, oldValue){
+				$scope.refreshOffset();
+	      		})
+	      		$scope.refreshOffset=function(){
+	      			$scope.offset=$scope.options.pagination.pageIndex * $scope.options.pagination.pageLength;
+	      		}
+	      		$scope.updateRecordsCount=function(){
+	      			$scope.options.pagination.pageLength=$scope.options.pagination.itemsPerPage.selected;
+	      			$scope.dataFilterSearch = $filter('filter')($scope.data,$scope.options.search.searchText);
+	      		}
+	      		$scope.$watch('options.pagination.pageLength', function(newValue, oldValue){
+				$scope.pages= new Array(Math.ceil($scope.dataFilterSearch.length/newValue));
+	      		})
+	      		$scope.$watch('options.search.searchText', function(newValue, oldValue){
+	      			$scope.dataFilterSearch = $filter('filter')($scope.data,newValue);
+	      		})
+	      		$scope.$watch('dataFilterSearch.length', function(newValue, oldValue){
+	      			$scope.pages= new Array(Math.ceil(newValue/$scope.options.pagination.pageLength));
+	      		})
+	      		
+			
+		}
+	}
+});
+
+
+angular.module("bls_tpls", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put('template/blsGrid/blsGrid.html',
+  	'<pre>pageLength = {{options.pagination.pageLength}} pageIndex : {{options.pagination.pageIndex}} offset = {{offset}} Sorting predicate = {{predicate}}; reverse = {{reverse}}</pre>\
+		 <div class="bls-table-container">\
+		 		<div class="row-fluid">\
+		 				<form action="" class="search-form">\
+		 		                		<div class="form-group has-feedback">\
+		 		            				<label for="search" class="sr-only">Search</label>\
+		 		            				<input type="text" class="{{options.search.searchClass}}" name="search" id="search" placeholder="search" ng-model="options.search.searchText">\
+		 		              			<span class="glyphicon glyphicon-search form-control-feedback"></span>\
+		 		            			</div>\
+		 		            		</form>\
+		 		 </div>\
+			<div ng-class="{\'overlay\':isLoading}"><div ng-show="isLoading"><div class="double-bounce1"></div>  <div class="double-bounce2"></div></div></div>\
+			<table class="{{gridClass}} blsGrid">\
+	      			<thead>\
+	        			<tr>\
+	          				<th class="colHeader" ng-repeat="col in columns"  ng-click="order(col)">{{col|uppercase}}\
+	          				<i ng-class="glyphOrder(col)" class="glyphicon pull-right"></i></th>\
+	        			</tr>\
+	      			</thead>\
+	      			<tbody>\
+	        				<tr ng-repeat="d in filteredData = (data | filter:options.search.searchText| limitTo:options.pagination.pageLength:offset | orderBy:predicate:reverse)">\
+	        					<td ng-repeat="a in columns">{{d[a]}} </td>\
+	        				</tr>\
+	        			</tbody></table>\
+	        			<nav>\
+		        			<ul class="pagination col-md-10 col-xs-8">\
+		        				<li ng-class="{ disabled:options.pagination.pageIndex == 0}">\
+						      <a ng-show="options.pagination.pageIndex!=0" href="#" aria-label="{{options.pagination.pager.perviousTitle}}" ng-click="toPage(options.pagination.pageIndex-1)">\
+						        <span aria-hidden="true">&laquo;</span>\
+						      </a>\
+						      <span ng-show="options.pagination.pageIndex==0" aria-hidden="true">&laquo;</span>\
+						 </li>\
+		        				<li ng-class="{active:$index==options.pagination.pageIndex}" ng-repeat="page in pages track by $index">\
+		        					<a ng-click="toPage($index)">{{$index+1}}</a></li>\
+		        				<li ng-class="{disabled:options.pagination.pageIndex==(pages.length-1)}">\
+						      <a ng-show="options.pagination.pageIndex!=(pages.length-1)" href="#" aria-label="{{options.pagination.pager.nextTitle}}" ng-click="toPage(options.pagination.pageIndex+1)">\
+						        <span aria-hidden="true">&raquo;</span>\
+						      </a>\
+						      <span ng-show="options.pagination.pageIndex==(pages.length-1)" aria-hidden="true">&raquo;</span>\
+						</li>\
+		        			</ul>\
+		        			<div class="pagerList col-md-2 col-xs-4">\
+		  				<select class="form-control" id="sel1" ng-model="options.pagination.itemsPerPage.selected" ng-change="updateRecordsCount()" ng-options="c as c for c in options.pagination.itemsPerPage.range" ng-selected="options.pagination.itemsPerPage.selected == c"></select>\
+					</div>\
+	        			</nav>\
+	        		</div>');
+}]);
