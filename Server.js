60b6d7c9069335397a6e805fdd47df013bf41dc3
Heni <tunisienheni@gmail.com[gui]>
2015-07-12 18:03:16 +0200

    HFA: mise en place d'un serveur de test NodeJs and ngViews for testing diffrents blsGrid types

---
+++ Server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+app.use('/dist',express.static('dist'));
+app.use('/app',express.static('app'));
+app.use('/Content',express.static('Content'));
+app.use('/font-awesome-4.3.0',express.static('font-awesome-4.3.0'));
+app.use('/bower_components',express.static('bower_components'));
+app.use('/Views',express.static('Views'));
+
+//Store all HTML files in view folder.
+app.get('/favicon.ico',function(req,res){
+  res.sendFile(path.join(__dirname+'/favicon.ico'));
+  //__dirname : It will resolve to your project folder.
+});
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(3001);
+
+console.log("Running at Port 3001");
