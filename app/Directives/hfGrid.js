b025087a27d8d3f590c80a9dd50265966578f82a
Heni <tunisienheni@gmail.com[gui]>
2015-07-19 21:04:50 +0200

    fix some bugs

--- app/Directives/hfGrid.js
+++ app/Directives/hfGrid.js
@@ -1,96 +1,87 @@
-app.directive('hfGrid', ['$log', '$templateRequest', '$compile', function($log, $templateRequest, $compile) {
+app.directive('hfGrid', ['$log', '$templateRequest', '$compile', 'localStorageService', function($log, $templateRequest, $compile, localStorageService) {
+    var link = {
+        post: function(scope, element, attrs, ctrls) {
+            $log.debug('_______in post hfGrid');
+        }
+    };
     var ctrl = function($scope, $element, $log, $templateRequest, $compile) {
         $log.debug('in hfGrid');
+        var me = this;
         $scope.rows = $scope.cols = [];
         $scope.uniqueId = 'hfGrid_' + $element[0].id;
-        $scope.pagination = {};
+        $scope.sort = $scope.pagination = {};
+        $scope.isSortInited = false;
         $scope.core = {
-            isLoading: true
+            isLoading: true,
+            isFirstLoad: true
         };
-        $scope.$on('hfGridStartDataLoadingEvent', function(e) {
+        this.reloadData = function() {
             $scope.core.isLoading = true;
-        });
-        $scope.$on('hfGridEndDataLoadingEvent', function(e, cols, rows) {
-            $log.debug('hfGridEndDataLoadingEvent intercepted');
-            $scope.cols = cols;
-            $scope.rows = rows;
-            $templateRequest('templates/hfGridTpl.html').then(function(response) {
-                $log.debug('start compile');
-                // compile the html, then link it to the scope
-                elem = $compile(response)($scope);
-                // append the compiled template inside the element
-                $element.html(elem);
+            $scope.source()($scope.pagination.pageIndex, $scope.pagination.itemsPerPage, $scope.pagination.searchedText, $scope.sort.orderBy, $scope.sort.order).then(function(res) { //query: pageIndex, pageLength, searchedText, orderBy, order
+                $log.debug('dataSource => ', res);
+                $scope.rows = res.data;
                 $scope.core.isLoading = false;
+                if ($scope.core.isFirstLoad) {
+                    $templateRequest('templates/hfGridTpl.html').then(function(response) {
+                        $log.debug('**************************  start compile');
+                        // compile the html, then link it to the scope
+                        elem = $compile(response)($scope);
+                        // append the compiled template inside the element
+                        $element.html(elem);
+                        $scope.core.isFirstLoad = false;
+                    });
+                }
             });
-        });
-        this.setPagination = function(pagination) {
+        }
+        $scope.$on('hfGridPaginationChanged', function(e, pagination) {
+            $log.debug('on hfGridPaginationChanged intercepted');
             $scope.pagination = pagination;
-        };
-        // $scope.order = function(predicate) {
-        //     $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
-        //     $scope.predicate = predicate;
-        //     // $scope.saveUserData({
-        //     //     key: $scope.storageIds.predicateId,
-        //     //     val: $scope.predicate
-        //     // });
-        //     // $scope.saveUserData({
-        //     //     key: $scope.storageIds.reverseId,
-        //     //     val: $scope.reverse
-        //     // });
-        //     // refreshDataGrid();
-        // };
-        // $scope.glyphOrder = function(col) {
-        //     //$log.info('glyphOrder function was called');
-        //     if (col != $scope.predicate) return '';
-        //     $scope.reverse = localStorageService.get($scope.storageIds.reverseId) || $scope.reverse;
-        //     return $scope.reverse ? 'fa-sort-asc' : 'fa-sort-desc';
-        // };
-    };
-    return {
-        controller: ctrl,
-        replace: true,
-        restrict: 'E'
-            //templateUrl: 'templates/hfGridTpl.html'
-    };
-}]).directive('hfGridBody', ['$log', function($log) {
-    var link = function(scope, element, attrs, ctrlGridBody) {
-        $log.debug('link hfGrid Body');
-    };
-    var ctrl = function($scope, $element, $log) {
-        $scope.rows = $scope.cols = [];
-        var me = this;
+            //fire refresh data
+            me.reloadData();
+        });
+        $scope.$on('hfGridSortChanged', function(e, sort) {
+            $log.debug('on hfGridSortChanged intercepted');
+            $scope.sort = sort;
+            //fire refresh data
+            me.reloadData();
+        });
+        this.setSort = function(sort) {
+            if (sort != $scope.sort) {
+                $log.debug('set Sort');
+                $scope.sort = sort;
+                //fire refresh data
+                //me.reloadData();
+            }
+        }
+        this.getSort = function() {
+            return $scope.sort;
+        }
+       
         this.setCols = function(cols) {
             $scope.cols = cols;
             $log.debug('settings cols => ', $scope.cols)
         };
-        $scope.$on('hfGridPaginationChanged', function(e, pagination) {
-            $log.debug('hfGridPaginationChanged intercepted');
-            me.setPagination(pagination);
-        });
-        this.setPagination = function(pagination) {
-            $scope.$emit('hfGridStartDataLoadingEvent'); //Fire Loading
-            $scope.pagination = pagination;
-            $scope.source()($scope.pagination.pageIndex, $scope.pagination.itemsPerPage, $scope.pagination.searchedText, $scope.pagination.orderBy, $scope.pagination.order).then(function(res) { //query: pageIndex, pageLength, searchedText, orderBy, order
-                $log.debug('dataSource => ', res);
-                $scope.rows = res.data;
-                $scope.$emit('hfGridEndDataLoadingEvent', $scope.cols, $scope.rows);
-            });
+        this.getIsSortInited = function() {
+            return $scope.isSortInited;
+        }
+        this.setIsSortInited = function(val) {
+            $scope.isSortInited = val;
         }
     };
     return {
-        require: 'hfGridBody',
+        controller: ctrl,
+        restrict: 'E',
+        link: link,
         scope: {
             source: '&'
         },
-        link: link,
-        controller: ctrl,
-        restrict: 'E',
+        //templateUrl: 'templates/hfGridTpl.html'
     };
 }]).directive('hfGridColumns', ['$log', function($log) {
     var link = function(scope, element, attrs, controllers) {
-        var hfGridBodyCtrl = controllers[0];
+        var hfGridCtrl = controllers[0];
         var hfGridColumnsCtrl = controllers[1];
-        hfGridBodyCtrl.setCols(hfGridColumnsCtrl.getCols());
+        hfGridCtrl.setCols(hfGridColumnsCtrl.getCols());
         $log.debug('link hfGridColumns');
         //controllers[1].initCols(ctrlhfGrid.sourceFunc);
     };
@@ -105,7 +96,7 @@ app.directive('hfGrid', ['$log', '$templateRequest', '$compile', function($log, 
         };
     };
     return {
-        require: ['^hfGridBody', 'hfGridColumns'],
+        require: ['^hfGrid', 'hfGridColumns'],
         link: link,
         controller: ctrl
     };
@@ -130,81 +121,101 @@ app.directive('hfGrid', ['$log', '$templateRequest', '$compile', function($log, 
 }]).directive('hfGridSortable', ['$log', '$compile', function($log, $compile) {
     //<i ng-if="col.sortable" class="pull-left fa fa-sort " ng-class="glyphOrder(col.id)"></i>
     var link = {
-        post: function(scope, element, attrs, hfGridSortableCtrl) {
+        pre: function(scope, element, attrs, ctrls) {
+            $log.debug('=> pre link hfGridSortable');
+            element.removeAttr('hf-grid-sortable');
+        },
+        post: function(scope, element, attrs, ctrls) {
+            $log.debug('=> post link hfGridSortable');
+            var hfGridCtrl = ctrls[0];
+            var hfGridSortableCtrl = ctrls[1];
+            element.on("click", function() {
+                scope.onSort(scope.col);
+            });
             //init order user
-            hfGridSortableCtrl.init();
-            $log.debug('================>> Link hfGridSortable');
+            
+            if (!hfGridCtrl.getIsSortInited()) {
+                $log.debug('Sort is not inited !!!!!');
+                hfGridSortableCtrl.init();
+                hfGridCtrl.setIsSortInited(true);
+                hfGridCtrl.setSort(scope.sort);
+            } else {
+                scope.sort = hfGridCtrl.getSort();
+            }
+            // $log.debug('================>> Link hfGridSortable sort =>', scope.sort);
             var sortElement = angular.element('<i ng-if="col.sortable" class="pull-left fa fa-sort " ng-click="onSort(col)" ng-class="glyphOrder(col)"></i>');
             element.append($compile(sortElement)(scope));
         }
     };
     var ctrl = function($scope, $element, $log, localStorageService) {
+        var me = this;
         var orderByKey = '_orderBy';
         var orderKey = '_order';
+        $scope.sort = {};
         $scope.onSort = function(col) {
-            $log.debug('sorting on ', col);
-            $scope.pagination.order = col.field == $scope.pagination.orderBy ? !$scope.pagination.order : $scope.pagination.order;
-            $scope.pagination.orderBy = col.field;
-            saveUserData();
-            $scope.$broadcast('hfGridPaginationChanged', $scope.pagination);
+            $log.debug('sorting on ', col.field);
+            $scope.sort.order = col.field == $scope.sort.orderBy ? !$scope.sort.order : $scope.sort.order;
+            $scope.sort.orderBy = col.field;
+            me.saveUserData();
+            $scope.$emit('hfGridSortChanged', $scope.sort);
         };
         $scope.glyphOrder = function(col) {
-            if (col.field != $scope.pagination.orderBy) return '';
-            return $scope.pagination.order ? 'fa-sort-asc' : 'fa-sort-desc';
+            $log.debug('glyphOrder => sort', $scope.sort, col.field);
+            if (col.field != $scope.sort.orderBy) return '';
+            return $scope.sort.order ? 'fa-sort-asc' : 'fa-sort-desc';
         };
-        var saveUserData = function() {
+        this.saveUserData = function() {
             if (localStorageService.isSupported) {
-                localStorageService.set($scope.uniqueId + orderByKey, $scope.pagination.orderBy);
-                localStorageService.set($scope.uniqueId + orderKey, $scope.pagination.order);
+                localStorageService.set($scope.uniqueId + orderByKey, $scope.sort.orderBy);
+                localStorageService.set($scope.uniqueId + orderKey, $scope.sort.order);
             }
         };
         this.init = function() {
             if (localStorageService.isSupported) {
-                $scope.pagination.orderBy = localStorageService.get($scope.uniqueId + orderByKey);
-                $scope.pagination.order = localStorageService.get($scope.uniqueId + orderKey);
+                $scope.sort.orderBy = localStorageService.get($scope.uniqueId + orderByKey);
+                $scope.sort.order = localStorageService.get($scope.uniqueId + orderKey);
             }
+            // $log.debug('init sort =>', $scope.sort);
         }
     };
     return {
-        require: 'hfGridSortable',
+        require: ['^hfGrid', 'hfGridSortable'],
         link: link,
         restrict: 'A',
-        controller: ctrl
+        controller: ctrl,
+        priority:-10
     };
 }]).directive('hfPagination', ['$log', function($log) {
     var link = function(scope, element, attrs, ctrls) {
         $log.debug('in link hfPagination');
         var hfGridCtrl = ctrls[0];
-        var hfGridBodyCtrl = ctrls[1];
         scope.pagination = {
             pageIndex: 1,
             searchedText: '',
-            orderBy: 'id',
-            order: 0
+            totalItems: false
         };
-        scope.pagination.itemsPerPage = scope.$eval(attrs.itemsPerPage);
-        scope.pagination.range = scope.$eval(attrs.range);
-        scope.pagination.totalItems = scope.$eval(attrs.totalItems);
-        scope.pagination.maxSize = scope.$eval(attrs.maxSize);
-        hfGridCtrl.setPagination(scope.pagination);
-        hfGridBodyCtrl.setPagination(scope.pagination);
         attrs.$observe('totalItems', function(newVal, oldVal) {
             if (newVal != oldVal) {
                 scope.pagination.totalItems = newVal;
-                hfGridCtrl.setPagination(scope.pagination);
+                scope.pagination.itemsPerPage = scope.$eval(attrs.itemsPerPage);
+                scope.pagination.range = scope.$eval(attrs.range);
+                scope.pagination.totalItems = scope.$eval(attrs.totalItems);
+                scope.pagination.maxSize = scope.$eval(attrs.maxSize);
+                $log.debug('pagination.totalItems => ', scope.pagination.totalItems);
+                //hfGridCtrl.setPagination(scope.pagination);
             }
         });
-        $log.debug('pagination.totalItems => ', scope.pagination.totalItems);
     };
     var ctrl = function($scope, $element, $log) {
         $scope.$watch('pagination', function(newVal, oldVal) {
+            if (!$scope.pagination.totalItems) return;
             if (newVal.pageIndex == oldVal.pageIndex) $scope.pagination.pageIndex = 1;
-            $log.debug('pagination changed');
+            $log.debug('pagination changed fired');
             $scope.$broadcast('hfGridPaginationChanged', $scope.pagination);
         }, true);
     };
     return {
-        require: ['^hfGrid', '^hfGridBody'],
+        require: ['^hfGrid'],
         link: link,
         restrict: 'E',
         controller: ctrl
@@ -220,4 +231,3 @@ app.directive('hfGrid', ['$log', '$templateRequest', '$compile', function($log, 
 //         controller: ctrl
 //     };
 // }])
-//
