1ef0273cb81abc71748b4662fef062ba73c0c43b
Heni <tunisienheni@gmail.com[gui]>
2015-08-02 22:16:22 +0200

    HFA: fix loading data

--- app/Controllers/testCtrl.js
+++ app/Controllers/testCtrl.js
@@ -99,45 +99,45 @@
                 }
             }
         };
-        $scope.query(1, 10);
+        //$scope.query(1, 10);
         $scope.cols = ['id', 'name', 'firstName', 'birthday', 'phone'];
-        $scope.data = [{
-            id: '1',
-            name: 'fazzani',
-            firstName: 'heni',
-            birthday: 1982,
-            phone: '0667426422'
-        }, {
-            id: '2',
-            name: 'fazzani2',
-            firstName: 'heni',
-            birthday: 1982,
-            phone: '0667426422'
-        }, {
-            id: '3',
-            name: 'fazzani3',
-            firstName: 'heni',
-            birthday: 1982,
-            phone: '0667426422'
-        }, {
-            id: '4',
-            name: 'fazzani4',
-            firstName: 'heni',
-            birthday: 1982,
-            phone: '0667426422'
-        }, {
-            id: '5',
-            name: 'fazzani5',
-            firstName: 'heni',
-            birthday: 1982,
-            phone: '0667426422'
-        }, {
-            id: '6',
-            name: 'fazzani',
-            firstName: 'heni',
-            birthday: 1980,
-            phone: '0667426422'
-        }];
+        // $scope.data = [{
+        //     id: '1',
+        //     name: 'fazzani',
+        //     firstName: 'heni',
+        //     birthday: 1982,
+        //     phone: '0667426422'
+        // }, {
+        //     id: '2',
+        //     name: 'fazzani2',
+        //     firstName: 'heni',
+        //     birthday: 1982,
+        //     phone: '0667426422'
+        // }, {
+        //     id: '3',
+        //     name: 'fazzani3',
+        //     firstName: 'heni',
+        //     birthday: 1982,
+        //     phone: '0667426422'
+        // }, {
+        //     id: '4',
+        //     name: 'fazzani4',
+        //     firstName: 'heni',
+        //     birthday: 1982,
+        //     phone: '0667426422'
+        // }, {
+        //     id: '5',
+        //     name: 'fazzani5',
+        //     firstName: 'heni',
+        //     birthday: 1982,
+        //     phone: '0667426422'
+        // }, {
+        //     id: '6',
+        //     name: 'fazzani',
+        //     firstName: 'heni',
+        //     birthday: 1980,
+        //     phone: '0667426422'
+        // }];
         $scope.cols = ['id', 'name', 'firstName', 'birthday', 'phone'];
         // $scope.getChildren = function(obj) {
         //     $log.debug('in getChildren...');
